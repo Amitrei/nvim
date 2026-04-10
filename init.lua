1ad2d9ec884858dbb56ff0a8a1f4021bea534e74
amit reinich <amit.reinich@gong.io>
2026-04-10 15:58:32 +0300

    feat(gcc): Added clang support LSP

--- init.lua
+++ init.lua
@@ -453,6 +453,7 @@ require('lazy').setup({
           end,
         },
         html = {},
+        clangd = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
